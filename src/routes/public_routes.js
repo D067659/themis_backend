5823c808c5fec46d2d559dedded36484e6942ce1
peter <peter.widmer@web.de>
2021-08-18 16:46:32 +0200

    added health check

--- src/routes/public_routes.js
+++ src/routes/public_routes.js
@@ -5,5 +5,8 @@ var playerController = require('../controller/player-controller');
 /* Authentication endpoints */
 routes.post('/login', playerController.loginPlayer);
 routes.post('/register', playerController.createPlayer);
+routes.get('/health', (req, res) => {
+    res.send('App is up and running!')
+})
 
 module.exports = routes;
