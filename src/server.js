27547b3583cfda7d371b079302d31e298c204ee1
peter <peter.widmer@web.de>
2021-08-18 16:34:29 +0200

    remove consolelog

--- src/server.js
+++ src/server.js
@@ -1,6 +1,4 @@
 require('dotenv').config({ path: '../.env' })
-console.log('hi', process.env.PORT)
 const app = require('./app');
 const port = process.env.PORT || 5000;
 app.listen(port);
-console.log('App is up and running at http://localhost:' + port)
