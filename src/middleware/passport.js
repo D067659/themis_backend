44c694ea93d712e57300191c06afa9e5cc59d8c7
peter <peter.widmer@web.de>
2021-08-18 16:41:09 +0200

    removed logs

--- src/middleware/passport.js
+++ src/middleware/passport.js
@@ -1,13 +1,11 @@
 var Player = require('../models/player');
 var JwtStrategy = require('passport-jwt').Strategy,
     ExtractJwt = require('passport-jwt').ExtractJwt;
-require('dotenv').config();
 
 var opts = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: process.env.JWT_SECRET
 }
-console.log(opts)
 
 module.exports = new JwtStrategy(opts, function (jwt_payload, done) {
     Player.findById(jwt_payload.id, function (err, player) {
