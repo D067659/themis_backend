9db7d810087f3dd3d52cb265718532a01d232b83
D067659 <peter.widmer@sap.com>
2022-02-01 14:44:36 +0100

    put back access control CORS topic

--- src/app.js
+++ src/app.js
@@ -9,6 +9,11 @@ var app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 passport.use(passportMiddleware);
