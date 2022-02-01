9f08d0cc6ee784aa28efb4e9d7a6d37b9a673112
D067659 <peter.widmer@sap.com>
2022-02-01 15:00:32 +0100

    try out

--- src/app.js
+++ src/app.js
@@ -8,13 +8,7 @@ var public_routes = require('./routes/public_routes');
 var app = express();
 
 app.use(express.json());
-app.use(cors());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://themis-sport.web.app");
-    res.header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization, Accept");
-    next();
-});
+// app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 passport.use(passportMiddleware);
