040456247540f3df2c77b75d915afb8cdfdf5036
D067659 <peter.widmer@sap.com>
2022-02-02 16:52:13 +0100

    removed headers

--- src/app.js
+++ src/app.js
@@ -9,13 +9,6 @@ var app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Headers', true);
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    next();
-});
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 passport.use(passportMiddleware);
