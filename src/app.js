cbee56f596eafd47a658601c9829072c1a4d57c2
D067659 <peter.widmer@sap.com>
2022-03-07 15:53:14 +0100

    max 60 requets per min

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var helmet = require('helmet');
 var passport = require('passport');
-var mongoose = require('mongoose');
-var cors = require('cors')
 var passportMiddleware = require('./middleware/passport');
+var cors = require('cors')
 var protected_routes = require('./routes/protected_routes');
 var public_routes = require('./routes/public_routes');
+var RateLimit = require('express-rate-limit');
 var app = express();
+
+// set up rate limiter: maximum of 60 requests per minute
+var limiter = new RateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 60
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
-
+app.use(limiter);
 app.use(passport.initialize());
 passport.use(passportMiddleware);
 
